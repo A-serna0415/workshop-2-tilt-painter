f585291555a754f4c53d18230cb3d168fb96aa3d
Andres Serna <andresserna9815@gmail.com>
2026-02-07 01:42:46 +0000

    another button change

--- public/sketch.js
+++ public/sketch.js
@@ -42,9 +42,9 @@ function setup() {
   ) {
     // iOS 13+
     askButton = createButton("Enable Motion");
-    askButton.position(width / 2, 16);
-    askButton.style("font-size", "44px");
-    askButton.style("padding", "40px 56px");
+    askButton.position(width / 3, 16);
+    askButton.style("font-size", "34px");
+    askButton.style("padding", "28px 48px");
     askButton.mousePressed(handlePermissionButtonPressed);
   } else {
     window.addEventListener("devicemotion", deviceMotionHandler, true);
